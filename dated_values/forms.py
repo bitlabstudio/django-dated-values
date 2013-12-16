32d708b1ab48c7ecb30f72eed9716dfe5f40c6d2
Daniel Kaufhold <daniel.kaufhold@bitmazk.com>
2013-12-16 15:40:25 +0100

    Simplified forms.
    The first approach was too django-incompatible. Passing kwargs from a formset
    of formsets is nearly impossible to implement without going too deep into the
    django internals.

--- dated_values/forms.py
+++ dated_values/forms.py
@@ -1,107 +1,99 @@
 """Forms of the dated_values app."""
-from dateutil.relativedelta import relativedelta
 from django import forms
 
+from dateutil.relativedelta import relativedelta
+
 from .models import DatedValue
+from . import settings
 
 
-class ValueForm(forms.ModelForm):
-    """Form to handle one single DatedValue instance."""
+class ValuesForm(forms.Form):
+    """Form to handle two weeks of DatedValue instances."""
 
     def __init__(self, obj, date, valuetype, *args, **kwargs):
         """
         :param obj: An object, that has values attached.
         :param date: A datetime date.
         :param valuetype: The DatedValueType, we are working on.
+        :param instances: A queryset of DatedValues.
 
         """
-        super(ValueForm, self).__init__(*args, **kwargs)
-        self.instance.type = valuetype
-        self.instance.date = date
-        self.instance.object_id = obj.id
-        self.fields['value'].required = False
-        self.allow_empty = True
-
-    def save(self, **kwargs):
-        if self.instance.value:
-            return super(ValueForm, self).save(**kwargs)
-        elif self.instance.id:
-            self.instance.delete()
-            return None
-
-    class Meta:
-        fields = ['value', ]
-        model = DatedValue
-
-
-class ValueFormset(forms.models.modelformset_factory(DatedValue, ValueForm)):
-    """Formset to for the ValueForm form."""
-
-    def __init__(self, obj, date, length, valuetype, *args, **kwargs):
-        """
-        :param obj: An object, that has values attached.
-        :param date: A datetime date.
-        :param length: The amount of ValueFormsets to display.
-        :param valuetype: The DatedValueType, we are working on.
-
-        """
-        self.extra = length
-        super(ValueFormset, self).__init__(*args, **kwargs)
+        super(ValuesForm, self).__init__(*args, **kwargs)
         values = DatedValue.objects.filter(
             type=valuetype, date__gte=date,
-            date__lt=date + relativedelta(days=length))
-        # we assign the forms ourselves, because django would always try to
-        # create the first set of forms from the queryset and then append the
-        # extra empty ones. We though want to mix create and update forms in a
-        # variable order.
-        for days in range(0, length):
-            form_data = {'value': self.data.get('{0}-{1}-value'.format(
-                self.prefix, days))}
-            value_date = date + relativedelta(days=days)
+            date__lt=date + relativedelta(days=settings.DISPLAYED_ITEMS))
+        self.valuetype = valuetype
+        self.obj = obj
+        self.instances = []
+        for i in range(0, settings.DISPLAYED_ITEMS):
+            current_date = date + relativedelta(days=i)
+            self.fields['value{0}'.format(i)] = forms.DecimalField(
+                required=False, decimal_places=self.valuetype.decimal_places,
+                widget=forms.TextInput(attrs={'class': 'dated-values-input'}))
             try:
-                instance = values.get(date=value_date)
+                instance = values.get(date=current_date)
             except DatedValue.DoesNotExist:
-                instance = None
-            form = ValueForm(obj, value_date, valuetype, instance=instance,
-                             data=form_data)
-            self.forms.append(form)
+                self.instances.append(DatedValue(
+                    type=valuetype, object_id=obj.id, date=current_date))
+                self.initial['value{0}'.format(i)] = ''
+            else:
+                self.initial['value{0}'.format(i)] = instance.value
+                self.instances.append(instance)
+
+    def save(self, **kwargs):
+        saved_instances = []
+        if self.prefix:
+            prefix = self.prefix + '-'
+        else:
+            prefix = ''
+        for i, instance in enumerate(self.instances):
+            value = self.data.get('{0}value{1}'.format(prefix, i), None)
+            if value:
+                instance.value = value
+                instance.save()
+                saved_instances.append(instance)
+            elif not value and instance.id is not None:
+                instance.delete()
+        return saved_instances
 
 
-class MultiTypeValueFormset(forms.formsets.formset_factory(ValueFormset)):
-    """A formset, that allows to add multiple ValueFormset in one go."""
-    def __init__(self, obj, date, length, valuetypes, *args, **kwargs):
+class MultiTypeValuesFormset(forms.formsets.formset_factory(ValuesForm)):
+    def __init__(self, obj, date, valuetypes, *args, **kwargs):
+        self.extra = 2
+        self.obj = obj
+        self.date = date
+        self.valuetypes = valuetypes
+        self.dates = [date + relativedelta(days=i) for i in range(
+            0, settings.DISPLAYED_ITEMS)]
+        super(MultiTypeValuesFormset, self).__init__(*args, **kwargs)
+
+    def _construct_form(self, i, **kwargs):
         """
-        :param obj: An object, that has values attached.
-        :param date: A datetime date.
-        :param length: The amount of ValueFormsets to display.
-        :param valuetypes: A list of DatedValueTypes
-
+        Instantiates and returns the i-th form instance in a formset.
         """
-        self.extra = len(valuetypes)
-        super(MultiTypeValueFormset, self).__init__(*args, **kwargs)
-        for valuetype in valuetypes:
-            self.formset_prefix = '{0}_{1}'.format(
-                valuetype.slug, str(valuetype.id))
-            formset = ValueFormset(obj, date, length, valuetype,
-                                   prefix=self.formset_prefix,
-                                   data=kwargs.get('data'))
-            self.forms.append(formset)
-
-    def full_clean(self):
-        """
-        Cleans all of self.data and populates self._errors.
-
-        since we tricked the form again to also process extra forms and empty
-        ones, we need to use self.extra and not total_form_count as the range
-        max below.
-
-        """
-        self._errors = []
-        for i in range(0, self.extra):
-            form = self.forms[i]
-            self._errors.append(form.errors)
+        defaults = {
+            'auto_id': self.auto_id,
+            'prefix': self.add_prefix(i),
+            'error_class': self.error_class,
+            'obj': self.obj,
+            'date': self.date,
+            'valuetype': self.valuetypes[i],
+        }
+        if self.is_bound:
+            defaults['data'] = self.data
+            defaults['files'] = self.files
+        # Allow extra forms to be empty.
+        if i >= self.initial_form_count():
+            defaults['empty_permitted'] = True
+        defaults.update(kwargs)
+        form = self.form(**defaults)
+        self.add_fields(form, i)
+        return form
 
     def save(self):
-        return_val = []
+        saved_instances = []
         for form in self.forms:
-            return_val.append(form.save())
+            saved = form.save()
+            if saved is not None:
+                saved_instances.extend(saved)
+        return saved_instances
--- dated_values/tests/forms_tests.py
+++ dated_values/tests/forms_tests.py
@@ -4,132 +4,72 @@ from django.utils.timezone import now
 
 from django_libs.tests.factories import UserFactory
 
-from ..forms import ValueForm, ValueFormset, MultiTypeValueFormset
+from ..forms import ValuesForm, MultiTypeValuesFormset
 from ..models import DatedValue
 from .factories import DatedValueTypeFactory
 
 
-class ValueFormTestCase(TestCase):
-    """Tests for the most basic ValueForm."""
+class ValuesFormTestCase(TestCase):
+    """Tests for the most basic ValuesForm."""
     longMessage = True
 
     def setUp(self):
-        self.data = {'value': '2.22'}
+        self.data = {}
+        for i in range(0, 14):
+            self.data.update({'value{0}'.format(i): '{0}.12'.format(i)})
         self.user = UserFactory()
         self.type = DatedValueTypeFactory()
 
-    def _test_form(self):
+    def test_form(self):
+        # test instantiation
+        form = ValuesForm(self.user, now(), self.type)
+
         # Tests for creating objects
-        form = ValueForm(self.user, now(), self.type, data=self.data)
+        form = ValuesForm(self.user, now(), self.type, data=self.data)
         self.assertTrue(form.is_valid(), msg=(
             'The form should be valid. Errors: {0}'.format(form.errors)))
         form.save()
-        self.assertEqual(DatedValue.objects.count(), 1, msg=(
+        self.assertEqual(DatedValue.objects.count(), 14, msg=(
             'After calling save, there are not the correct amount of dated'
             ' values in the database.'))
 
-        value = DatedValue.objects.get()
-        form = ValueForm(self.user, now(), self.type, instance=value,
-                         data=self.data)
+        form = ValuesForm(self.user, now(), self.type, data=self.data)
         self.assertTrue(form.is_valid(), msg=(
             'The form should be valid. Errors: {0}'.format(form.errors)))
 
         form.save()
-        self.assertEqual(DatedValue.objects.count(), 1, msg=(
-            'After calling save with an instance, there was not the correct'
-            ' amount of dated values in the database.'))
+        self.assertEqual(DatedValue.objects.count(), 14, msg=(
+            'After calling save with an instances in the database, there was'
+            ' not the correct amount of dated values in the database.'))
 
         # Tests for not creating and deleting objects
-        data = {'value': ''}
-        form = ValueForm(self.user, now(), self.type, instance=value,
-                         data=data)
+        data = self.data.copy()
+        data.update({'value1': ''})
+        form = ValuesForm(self.user, now(), self.type, data=data)
         self.assertTrue(form.is_valid(), msg=(
             'The form with instance should be valid with the blank value.'
             ' Errors: {0}'.format(form.errors)))
 
         form.save()
-        self.assertEqual(DatedValue.objects.count(), 0, msg=(
+        self.assertEqual(DatedValue.objects.count(), 13, msg=(
             'After calling save with an empty value and an instance, the form'
             ' should have deleted that instance.'))
 
-        data = {'value': ''}
-        form = ValueForm(self.user, now(), self.type, data=data)
+        data = self.data.copy()
+        data.update({'value1': ''})
+        form = ValuesForm(self.user, now(), self.type, data=data)
         self.assertTrue(form.is_valid(), msg=(
             'The form should be valid even with the value being blank.'
             ' Errors: {0}'.format(form.errors)))
 
         form.save()
-        self.assertEqual(DatedValue.objects.count(), 0, msg=(
+        self.assertEqual(DatedValue.objects.count(), 13, msg=(
             'After calling save, there are not the correct amount of dated'
             ' values in the database.'))
 
 
-class ValueFormsetTestCase(TestCase):
-    """Tests for the ValueFormset formset class."""
-    longMessage = True
-
-    def setUp(self):
-        self.data = {
-            'form-TOTAL_FORMS': u'0',
-            'form-INITIAL_FORMS': u'0',
-            'form-0-value': '0.22',
-            'form-1-value': '1.22',
-            'form-2-value': '2.22',
-            'form-3-value': '3.22',
-            'form-4-value': '4.22',
-            'form-5-value': '5.22',
-            'form-6-value': '6.22',
-        }
-        self.user = UserFactory()
-        self.type = DatedValueTypeFactory()
-
-    def _test_form(self):
-        # Tests for creating objects
-        form = ValueFormset(self.user, now(), 7, self.type, data=self.data)
-        self.assertTrue(form.is_valid(), msg=(
-            'The form should be valid. Errors: {0}'.format(form.errors)))
-        form.save()
-        self.assertEqual(DatedValue.objects.count(), 7, msg=(
-            'After calling save, there are not the correct amount of dated'
-            ' values in the database.'))
-
-        form = ValueFormset(self.user, now(), 7, self.type, data=self.data)
-        self.assertTrue(form.is_valid(), msg=(
-            'The form should be valid. Errors: {0}'.format(form.errors)))
-
-        form.save()
-        self.assertEqual(DatedValue.objects.count(), 7, msg=(
-            'After calling save on the form, where we have instances in the'
-            ' database, there should still be the same instances in the'
-            ' database.'))
-
-        # Tests for not creating and deleting objects
-        data = self.data.copy()
-        data.update({'form-0-value': '', 'form-4-value': ''})
-        form = ValueFormset(self.user, now(), 7, self.type, data=data)
-        self.assertTrue(form.is_valid(), msg=(
-            'The form should be valid with the blank values.'
-            ' Errors: {0}'.format(form.errors)))
-
-        form.save()
-        self.assertEqual(DatedValue.objects.count(), 5, msg=(
-            'After calling save on the formset with 2 empty values and'
-            ' persistent values in th database, there should be a decreased'
-            ' amount of values in the database.'))
-
-        form = ValueFormset(self.user, now(), 7, self.type, data=data)
-        self.assertTrue(form.is_valid(), msg=(
-            'The form should be valid even with some values being blank.'
-            ' Errors: {0}'.format(form.errors)))
-
-        form.save()
-        self.assertEqual(DatedValue.objects.count(), 5, msg=(
-            'When we call save again with the same data, there should be'
-            ' the same amount of values in the database.'))
-
-
-class MultiTypeValueFormsetTestCase(TestCase):
-    """Tests for the MultiTypeValueFormset formset class."""
+class MultiTypeValuesFormsetTestCase(TestCase):
+    """Tests for the MultiTypeValuesFormset formset class."""
     longMessage = True
 
     def setUp(self):
@@ -137,51 +77,36 @@ class MultiTypeValueFormsetTestCase(TestCase):
         self.type2 = DatedValueTypeFactory()
         self.types = [self.type1, self.type2]
         self.user = UserFactory()
-        self.prefix1 = '{0}_{1}'.format(self.type1.slug, self.type1.id)
-        self.prefix2 = '{0}_{1}'.format(self.type2.slug, self.type2.id)
         self.data = {
-            'form-TOTAL_FORMS': u'0',
-            'form-INITIAL_FORMS': u'0',
-
-            '{0}-TOTAL_FORMS'.format(self.prefix1): u'0',
-            '{0}-INITIAL_FORMS'.format(self.prefix1): u'0',
-            '{0}-0-value'.format(self.prefix1): '0.22',
-            '{0}-1-value'.format(self.prefix1): '1.22',
-            '{0}-2-value'.format(self.prefix1): '2.22',
-            '{0}-3-value'.format(self.prefix1): '3.22',
-            '{0}-4-value'.format(self.prefix1): '4.22',
-            '{0}-5-value'.format(self.prefix1): '5.22',
-            '{0}-6-value'.format(self.prefix1): '6.22',
-
-            '{0}-TOTAL_FORMS'.format(self.prefix2): u'0',
-            '{0}-INITIAL_FORMS'.format(self.prefix2): u'0',
-            '{0}-0-value'.format(self.prefix2): '0.22',
-            '{0}-1-value'.format(self.prefix2): '1.22',
-            '{0}-2-value'.format(self.prefix2): '2.22',
-            '{0}-3-value'.format(self.prefix2): '3.22',
-            '{0}-4-value'.format(self.prefix2): '4.22',
-            '{0}-5-value'.format(self.prefix2): '5.22',
-            '{0}-6-value'.format(self.prefix2): '6.22',
+            'form-TOTAL_FORMS': u'2',
+            'form-INITIAL_FORMS': u'2',
         }
+        for i in range(0, 2):
+            for j in range(0, 14):
+                self.data.update({
+                    'form-{0}-value{1}'.format(i, j): '{0}.12'.format(j)})
 
     def test_form(self):
+        # test form instantiation
+        form = MultiTypeValuesFormset(self.user, now(), self.types)
+
         # Tests for creating objects
-        form = MultiTypeValueFormset(self.user, now(), 7, self.types,
-                                     data=self.data)
+        form = MultiTypeValuesFormset(self.user, now(), self.types,
+                                      data=self.data)
         self.assertTrue(form.is_valid(), msg=(
             'The form should be valid. Errors: {0}'.format(form.errors)))
         form.save()
-        self.assertEqual(DatedValue.objects.count(), 14, msg=(
+        self.assertEqual(DatedValue.objects.count(), 28, msg=(
             'After calling save, there are not the correct amount of dated'
             ' values in the database.'))
 
-        form = MultiTypeValueFormset(self.user, now(), 7, self.types,
-                                     data=self.data)
+        form = MultiTypeValuesFormset(self.user, now(), self.types,
+                                      data=self.data)
         self.assertTrue(form.is_valid(), msg=(
             'The form should be valid. Errors: {0}'.format(form.errors)))
 
         form.save()
-        self.assertEqual(DatedValue.objects.count(), 14, msg=(
+        self.assertEqual(DatedValue.objects.count(), 28, msg=(
             'After calling save on the form, where we have instances in the'
             ' database, there should still be the same instances in the'
             ' database.'))
@@ -189,30 +114,37 @@ class MultiTypeValueFormsetTestCase(TestCase):
         # Tests for not creating and deleting objects
         data = self.data.copy()
         data.update({
-            '{0}-0-value'.format(self.prefix1): '',
-            '{0}-1-value'.format(self.prefix1): '',
-            '{0}-0-value'.format(self.prefix2): '',
-            '{0}-1-value'.format(self.prefix2): '',
+            'form-0-value1': '',
+            'form-1-value1': '',
         })
-        form = MultiTypeValueFormset(self.user, now(), 7, self.types,
-                                     data=data)
+        form = MultiTypeValuesFormset(self.user, now(), self.types,
+                                      data=data)
         self.assertTrue(form.is_valid(), msg=(
             'The form should be valid with the blank values.'
             ' Errors: {0}'.format(form.errors)))
 
         form.save()
-        self.assertEqual(DatedValue.objects.count(), 10, msg=(
+        self.assertEqual(DatedValue.objects.count(), 26, msg=(
             'After calling save on the formset with 4 empty values and'
             ' persistent values in th database, there should be a decreased'
             ' amount of values in the database.'))
 
-        form = MultiTypeValueFormset(self.user, now(), 7, self.types,
-                                     data=data)
+        form = MultiTypeValuesFormset(self.user, now(), self.types,
+                                      data=data)
         self.assertTrue(form.is_valid(), msg=(
             'The form should be valid even with some values being blank.'
             ' Errors: {0}'.format(form.errors)))
 
         form.save()
-        self.assertEqual(DatedValue.objects.count(), 10, msg=(
+        self.assertEqual(DatedValue.objects.count(), 26, msg=(
             'When we call save again with the same data, there should be'
             ' the same amount of values in the database.'))
+
+        # Tests for invalid data
+        data = self.data.copy()
+        data.update({
+            'form-0-value1': '12.012312',
+        })
+        form = MultiTypeValuesFormset(self.user, now(), self.types,
+                                      data=data)
+        self.assertFalse(form.is_valid(), msg='The form should not be valid.')
